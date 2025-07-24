3593b931c63255cb24ada18269a5fb442acb43f2
urban-calderon <urbanocalderon@outlook.com>
2025-07-24 16:40:29 -0600

    feat: configuration deploy vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   }
